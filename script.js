b197393345faf5c3a967b09aebefd02efe50c483
Deepsky709 <ferdinand.amadeus123@gmail.com>
2025-02-06 17:20:52 +0700

    1.4

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let py = document.querySelector("#thing");
+let py = document.querySelector(".t1");
+let js = document.querySelector(".t2");
+let rs = document.querySelector(".t3");
+let rn = document.querySelector(".t4");
 
-
-
-let animation = gsap.to("#lang", {
+let animation = gsap.to(".l1", {
     y: 135,
     duration: 0.5,
     paused: true,
@@ -10,3 +11,38 @@ let animation = gsap.to("#lang", {
 
 py.addEventListener("mouseenter", () => animation.play());
 py.addEventListener("mouseleave", () => animation.reverse());
+
+let animation2 = gsap.to(".l2", {
+    y: 135,
+    duration: 0.5,
+    paused: true,
+});
+
+js.addEventListener("mouseenter", () => animation2.play());
+js.addEventListener("mouseleave", () => animation2.reverse());
+
+let animation3 = gsap.to(".l3", {
+    y: 135,
+    duration: 0.5,
+    paused: true,
+});
+
+rs.addEventListener("mouseenter", () => animation3.play());
+rs.addEventListener("mouseleave", () => animation3.reverse());
+
+let animation4 = gsap.to(".l4", {
+    y: 135,
+    duration: 0.5,
+    paused: true,
+});
+
+rn.addEventListener("mouseenter", () => animation4.play());
+rn.addEventListener("mouseleave", () => animation4.reverse());
+
+function aboutMe(){
+    window.location.href = 'index.html';
+}
+
+function pyThon(){
+    window.location.href = 'python.html';
+}
