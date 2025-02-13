3b1cc16ac1f707185ac94cd4de18bc96208498ed
Deepsky709 <ferdinand.amadeus123@gmail.com>
2025-02-13 17:03:17 +0700

    1.6

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let js = document.querySelector(".t2");
 let rs = document.querySelector(".t3");
 let rn = document.querySelector(".t4");
 
+
 let animation = gsap.to(".l1", {
     y: 135,
     duration: 0.5,
@@ -45,6 +46,7 @@ function aboutMe(){
 
 function pyThon(){
     window.location.href = 'python.html';
+    
 }
 
 function javaS(){
@@ -53,4 +55,49 @@ function javaS(){
 
 function lua(){
     window.location.href = 'lua.html';
+}
+
+function test(){
+    window.location.href = 'test.html';
+}
+
+function intro(){
+    gsap.from('#about', {
+        x: -150,
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from('.t1', {
+        y: 200,
+        opacity: 0,
+        duration: 0.8,
+    });
+    gsap.from('.t2', {
+        y: 200,
+        opacity: 0,
+        duration: 1.1,
+    });
+    gsap.from('.t3', {
+        y: 200,
+        opacity: 0,
+        duration: 1.4,
+    });
+    gsap.from('.t4', {
+        y: 200,
+        opacity: 0,
+        duration: 1.7,
+    });
+}
+
+function intro2(numb1, numb2){
+    gsap.from('.I'+numb1, {
+        y: 200,
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from('.I'+numb2, {
+        y: 200,
+        opacity: 0,
+        duration: 1.5,
+    });
 }
